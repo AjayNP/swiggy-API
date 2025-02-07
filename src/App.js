a461d3099ad1c574a0fdb1c08ef093823101c2ba
AjayNP <ajay.mca02@gmail.com>
2025-02-07 18:16:48 +0530

    Proxy updated with crossoriginme

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
 
   const fetchData = async () => {
     const mainData = await fetch(
-      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.7040592&lng=77.10249019999999&collection=83633&tags=layout_CCS_NorthIndian&sortBy=&filters=&type=rcv2&offset=0&page_type=null"
+      "https://crossorigin.me/https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.7040592&lng=77.10249019999999&collection=83633&tags=layout_CCS_NorthIndian&sortBy=&filters=&type=rcv2&offset=0&page_type=null"
     );
     const json = await mainData.json();
     const extractedData = json?.data?.cards?.slice(3).map((item) => ({
