e10e19be6930e627741f876554a5501264cab8e7
AjayNP <ajay.mca02@gmail.com>
2025-02-07 09:31:08 +0530

    Proxy updated

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,10 +1,12 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function(app) {
-    app.use('https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.7040592&lng=77.10249019999999&collection=83633&tags=layout_CCS_NorthIndian&sortBy=&filters=&type=rcv2&offset=0&page_type=null',
+    app.use(
+        'https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.7040592&lng=77.10249019999999&collection=83633&tags=layout_CCS_NorthIndian&sortBy=&filters=&type=rcv2&offset=0&page_type=null', // This should match the base path of your API requests in React
         createProxyMiddleware({
-            target: 'https://swiggyapitest.netlify.app/',
+            target: 'https://swiggyapitest.netlify.app/', // Change this to your backend server URL
             changeOrigin: true,
+            pathRewrite: { '^/api': '' }, // Optionally rewrite the API path
         })
     );
 };
