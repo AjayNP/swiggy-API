a678d4868764caf5e02381f69a88699a66fa7477
AjayNP <ajay.mca02@gmail.com>
2025-02-07 09:16:40 +0530

    Proxy added

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,7 +2,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function(app) {
     app.use(
-        '/https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.7040592&lng=77.10249019999999&collection=83633&tags=layout_CCS_NorthIndian&sortBy=&filters=&type=rcv2&offset=0&page_type=null',
+        'https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.7040592&lng=77.10249019999999&collection=83633&tags=layout_CCS_NorthIndian&sortBy=&filters=&type=rcv2&offset=0&page_type=null',
         createProxyMiddleware({
             target: 'https://swiggyapitest.netlify.app/',
             changeOrigin: true,
